c9c42b3310ea21a523702a11db6572605ccb8d69
Samuel Eagan <samueleagan@gmail.com>
2020-04-29 18:13:58 -0500

    just about done with this

--- eagan_arrayptr.c
+++ eagan_arrayptr.c
@@ -13,8 +13,9 @@ int main(void) {
     }
 
     printf("\n");
-    index = 10;
-    for (int *i = arrEnd - 1; *i >= arr; *i--) {
+
+    index = 9;
+    for (int i = arrEnd - 1; i >= arr; i--) {
         *(arr + index) = 2 * (*(arr + index)) + 4;
         printf("%5d", *(arr + index));
         index--;
