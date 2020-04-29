607158d1918500704c0714f8a0e7f4fdc381dee9
Samuel Eagan <samueleagan@gmail.com>
2020-04-29 17:44:28 -0500

    almost done with 2nd part

---
+++ eagan_arrayptr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int arr[10];
+    int index = 0;
+
+    int *arrEnd = arr + 10;
+    for (int *i = arr; i < arrEnd; *i++) {
+        *(arr + index) = index;
+        printf("%5d", *(arr + index));
+        index++;
+    }
+
+    printf("\n");
+    index = 10;
+    for (int *i = arrEnd - 1; *i >= arr; *i--) {
+        *(arr + index) = 2 * (*(arr + index)) + 4;
+        printf("%5d", *(arr + index));
+        index--;
+    }
+
+    printf("\n");
+    return 0;
+}
